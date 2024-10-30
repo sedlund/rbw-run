4472c8778ed477b952c6e95d61997adbffe6adf7
Scott Edlund <sedlund@users.noreply.github.com>
2024-10-30 05:00:04 +0000

    flake: nixpkgs -> upstream

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Run rbw in parallel to fetch secrets and set them as environment variables to run a program";
 
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    utils.url = "git+ssh://git@github.com/numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -21,7 +21,6 @@
         packages.default = pkgs.callPackage ./default.nix { };
         devShells.default = pkgs.mkShellNoCC {
           packages = with pkgs; [
-            nix-direnv
             nixfmt-rfc-style
             nufmt
             nushell
